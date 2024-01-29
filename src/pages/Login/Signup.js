1b513551265781479b15f1d3e5347df18058db4f
kowsalya260798 <153350280+kowsalya260798@users.noreply.github.com>
2024-01-29 07:48:44 +0530

    commit

--- src/pages/Login/Signup.js
+++ src/pages/Login/Signup.js
@@ -1,83 +1,134 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSiteContext } from '../../contexts/SiteProvider';
 import { post } from '../../services/smartApiService';
 import { Link, useNavigate } from 'react-router-dom';
+import croplogo from '../../assets/images/croplogo.png';
+import './Login.css'; // You can remove this if not needed
+import { LOGIN_lOGO } from '../../services/ImageService';
+
 
 const Signup = () => {
-  const { setLoading, setUser, startSessionAct } = useSiteContext();
+  const { setLoading, setUser, openModal, closeModal, startSessionAct } = useSiteContext();
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
 
-  const handleSignup = async () => {
-    try {
-      const response = await post('/signup', { email, password });
+  const handleLogin = () => {
+    const data = { username: 'kminchelle', password: '0lelplR' };
+    setLoading(true, 'Logging in Please Wait....');
+    const subscription = post('auth/login', data).subscribe((response) => {
+      setUser(response.data);
       setLoading(false);
-      setUser(response.user); 
-      startSessionAct(); 
-      navigate('/dashboard'); 
-    } catch (error) {
-      if (error.response) {
-       
-        setError(`Request failed with status code ${error.response.status}`);
-      } else if (error.request) {
-       
-        setError('No response received from the server');
-      } else {
-     
-        setError('Request failed to reach the server');
-      }
+      startSessionAct();
+      navigate('/');
+    });
+    return () => {
+      subscription.unsubscribe();
+    };
+  };
+
+  const handleRememberMe = () => {
+    if (rememberMe) {
+      localStorage.setItem('rememberedEmail', email);
+    } else {
+      localStorage.removeItem('rememberedEmail');
     }
   };
 
+  useEffect(() => {
+    const rememberedEmail = localStorage.getItem('rememberedEmail');
+    if (rememberedEmail) {
+      setEmail(rememberedEmail);
+      setRememberMe(true);
+    }
+  }, []);
+
+  const handleForgetPassword = () => {
+    // Implement your forget password logic here
+  };
+
+  const handleSignup = () => {
+    navigate('/signup');
+  };
+
   return (
-    <div className="signup-container mt-6">
-      <h1 className="signup-title">Sign Up</h1>
-      <div className="signup-field">
-        <label htmlFor="email" className="signup-label">
-          Email Address
-        </label>
-        <div className="signup-control">
-          <input
-            id="email"
-            className="signup-input"
-            type="text"
-            placeholder="Your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+    <section className="hero is-fullheight">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns is-vcentered">
+            <div className="left-column is-6 ">
+              <figure className="login-image ">
+                <img src={LOGIN_lOGO} alt="Login Logo" />
+              </figure>
+            </div>
+            <div className='login-part'>
+            <div className="right-column  is-6  ">
+              <div className="box">
+                <figure className="login-image">
+                  <img src={croplogo} alt="Crop Logo" />
+                </figure>
+                <h1 className="title is-4">Login</h1>
+                <p className="title is-6">Your Access to the World of Customer Loyalty</p>
+                <form onSubmit={handleLogin}>
+                  <div className="radio-field">
+                    <div className="radio-control">
+                      <label className="radio-label">
+                        <input type="radio" name="loginMethod" />
+                        Crop ID
+                      </label>
+                      <label className="radio-label">
+                        <input type="radio" name="loginMethod" />
+                        Mobile
+                      </label>
+                      <label className="radio-label">
+                        <input type="radio" name="loginMethod" />
+                        Email
+                      </label>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="Crop ID"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="password"
+                        placeholder="Pin"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="button-field">
+                    <div className="button-control">
+                      <button className="button-button" type="submit">
+                        Login
+                      </button>
+                    </div>
+                    <Link to="/signup" className="login-sign" onClick={handleSignup}>
+                      Not a Member? Signup Now
+                    </Link>
+                  </div>
+                  
+                </form>
+              </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="signup-field">
-        <label htmlFor="password" className="signup-label">
-          Password
-        </label>
-        <div className="signup-control">
-          <input
-            id="password"
-            className="signup-input"
-            type="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-      </div>
-      <div className="signup-field">
-        <button onClick={handleSignup} className="signup-button" type="button">
-          Sign Up
-        </button>
-        {error && <p className="signup-help">{error}</p>}
-      </div>
-      <p>
-        Already have an account?{' '}
-        <Link to="/login" className="signup-sign">
-          <u>Login here</u>
-        </Link>
-      </p>
-    </div>
-  );
-};
+    </section>
+);
+  };
 
 export default Signup;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,20 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { useSiteContext } from '../../contexts/SiteProvider';
 import { post } from '../../services/smartApiService';
 import { Link, useNavigate } from 'react-router-dom';
-//import back from '../../assets/images/background.avif';
+import croplogo from '../../assets/images/croplogo.png';
 import './Login.css'; // You can remove this if not needed
-import {LOGIN_lOGO} from "../../services/ImageService"
+import { LOGIN_lOGO } from '../../services/ImageService';
+import Signup from './Signup';
 
 const Login = () => {
   const { setLoading, setUser, openModal, closeModal, startSessionAct } = useSiteContext();
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
 
   const handleLogin = () => {
-    let data = { username: 'kminchelle', password: '0lelplR' };
+    const data = { username: 'kminchelle', password: '0lelplR' };
     setLoading(true, 'Logging in Please Wait....');
     const subscription = post('auth/login', data).subscribe((response) => {
       setUser(response.data);
@@ -44,156 +44,91 @@ const Login = () => {
     }
   }, []);
 
-  const MyFooterContent = () => {
-    return (
-      <div>
-        <button className="button is-success is-small">Save changes</button>
-        <button className="button is-small" onClick={closeModal}>Cancel</button>
-      </div>
-    );
-  };
-
-  const MyModalContent = () => {
-    return (
-      <div>
-        <p> This is everything ever </p>
-      </div>
-    );
-  };
-
-  const openMyModal = () => {
-    let modalObject = { title: 'login password', body: <MyModalContent />, footer: <MyFooterContent /> };
-    openModal(modalObject);
-  };
   const handleForgetPassword = () => {
-    const modalContent = (
-      <div>
-        <p>
-          <b>Forgot your password? Enter your email to reset it:</b>
-        </p>
-        <input
-          className="input is-primary"
-          type="text"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <button onClick={sendPasswordResetEmail} className="button is-primary">
-          Send Reset Email
-        </button>
-      </div>
-    );
-    const modalFooter = (
-      <div>
-        <button onClick={closeModal} className="button is-small">
-          Cancel
-        </button>
-      </div>
-    );
-    const modalObject = { title: 'Forgot Password', body: modalContent, footer: modalFooter };
-    openModal(modalObject);
+    // Implement your forget password logic here
   };
-  const sendPasswordResetEmail = () => {
-    closeModal();
 
-  };
   const handleSignup = () => {
     navigate('/signup');
   };
 
   return (
-    <div className="login-container">
-      <div className="login-column pt-6 pb-6 is-6">
-        <div className="login-box">
-          <h1 className="login-title is-4">Login</h1>
-          <div className="login-field">
-            <p className="login-acc">
-              Doesn't have an account yet?{' '}
-              <Link to="/signup" className="login-sign">
-                <u onClick={handleSignup}>Sign up</u>
-              </Link>
-            </p>
-          </div>
-          <div className="login-field">
-            <label htmlFor="email" className="login-label">
-              Email Address
-            </label>
-            <div className="login-control">
-              <input
-                id="email"
-                className="login-input"
-                type="text"
-                placeholder="You@example.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
+    <section className="hero is-fullheight">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns is-vcentered">
+            <div className="left-column is-6 ">
+              <figure className="login-image ">
+                <img src={LOGIN_lOGO} alt="Login Logo" />
+              </figure>
+            </div>
+            <div className='login-part'>
+            <div className="right-column  is-6  ">
+              <div className="box">
+                <figure className="login-image">
+                  <img src={croplogo} alt="Crop Logo" />
+                </figure>
+                <h1 className="title is-4">Login</h1>
+                <p className="title is-6">Your Access to the World of Customer Loyalty</p>
+                <form onSubmit={handleLogin}>
+                  <div className="radio-field">
+                    <div className="radio-control">
+                      <label className="radio-label">
+                        <input type="radio" name="loginMethod" />
+                        Crop ID
+                      </label>
+                      <label className="radio-label">
+                        <input type="radio" name="loginMethod" />
+                        Mobile
+                      </label>
+                      <label className="radio-label">
+                        <input type="radio" name="loginMethod" />
+                        Email
+                      </label>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="Crop ID"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="password"
+                        placeholder="Pin"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="button-field">
+                    <div className="button-control">
+                      <button className="button-button" type="submit">
+                        Login
+                      </button>
+                    </div>
+                    <Link to="/signup" className="login-sign" onClick={handleSignup}>
+                      Not a Member? Signup Now
+                    </Link>
+                  </div>
+                  
+                </form>
+              </div>
+              </div>
             </div>
           </div>
-          <div className="login-field">
-            <div className="login-control is-expanded">
-              <label htmlFor="password" className="login-label">
-                Password
-              </label>
-              <Link to="/forget" className="login-sign">
-                <u onClick={handleForgetPassword}>forgot password?</u>
-              </Link>
-              <input
-                id="password"
-                className="login-input"
-                type="password"
-                placeholder="Enter 6 Characters or more"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="login-field">
-            <label className="login-checkbox">
-              <input
-                className="login-checkbox"
-                type="checkbox"
-                checked={rememberMe}
-                onChange={(e) => {
-                  setRememberMe(e.target.checked);
-                  handleRememberMe();
-                }}
-              />
-              Remember Me
-            </label>
-          </div>
-          <div className="login-field">
-            <button onClick={handleLogin} className="login-button" type="button">
-              Login
-            </button>
-            {error && <p className="login-help">{error}</p>}
-          </div>
-
         </div>
       </div>
-      <div className="login-column pt-6 pb-6 is-6">
-        <div className="login-box">
-          <figure className="login-image">
-            <img src={LOGIN_lOGO} alt="captures" />
-          </figure>
-        </div>
-      </div>
-    </div>
-  );
-};
+    </section>
+);
+  };
 
 export default Login;
-/*<div className="social-button">
-<ul className="is-flex">
-  <li className="mr-2">
-    <button className="google button" type="button">
-      <i class="fa fa-google-plus-square" aria-hidden="true"></i> Google
-    </button>
-  </li>
-  <li>
-    <button className="facebook button" type="button">
-      <i class="fa fa-facebook-square" aria-hidden="true"></i> Facebook
-    </button>
-  </li>
-</ul>
-</div>*/
