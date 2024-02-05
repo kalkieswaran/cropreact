1ef2f546f48177fb27abe481fe01be8934c66d09
Balaji <139791811+Mr-Balaji2594@users.noreply.github.com>
2024-02-05 18:06:11 +0530

    ok push

--- src/services/ImageService.ts
+++ src/services/ImageService.ts
@@ -1,2 +1,3 @@
 export { default as SITE_lOGO } from "../assets/images/image.png";
 export { default as LOGIN_lOGO } from "../assets/images/securitylogin.png";
+export { default as FOLDER_ICON } from "../assets/images/foldericon.png";
