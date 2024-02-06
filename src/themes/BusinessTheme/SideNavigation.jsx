aef38460913c6ca0738455d42a9937a6ba694075
Janarthanan <139687402+jana050@users.noreply.github.com>
2024-02-06 11:45:58 +0530

    side nave update

--- src/themes/BusinessTheme/SideNavigation.jsx
+++ src/themes/BusinessTheme/SideNavigation.jsx
@@ -1,31 +1,70 @@
 import React, { useState } from 'react'
 import './SideNav.css' 
-const SideNavigation = () => {
+import { useLocation, useNavigate } from 'react-router-dom';
 
-    const [sidebarOpen, setSidebarOpen] = useState(false);
+const  ManageServices=()=>{
+  
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  
+  const handleNavigation = (link) => {
+    navigate(link);
+  };
 
-    const toggleSidebar = () => {
-      setSidebarOpen(sidebarOpen=>!sidebarOpen);
-      console.log('Toggle sidebar')
-    };
+  const toggleSidebar = () => {
+    setSidebarOpen(sidebarOpen=>!sidebarOpen);
+    console.log('Toggle sidebar')
+  };
 
-    const  toggleSidebarOpen= sidebarOpen ?"active":null;
-  return (
-   <>
-   <div className='main-side-nave-items'>
+  const ManageServicesItems=[
+    {
+      id: 1,
+      label: "My Offers",
+      icon: <i class="fa fa-trash" aria-hidden="true"></i>,
+      link:"/business/amendform"
+    },
+    {
+      id: 1,
+      label: "Create Offers",
+      icon: <i class="fa fa-pencil-square-o" aria-hidden="true"></i>,
+      link:"/business/createoffersform"
+    },
+    {
+      id: 1,
+      label: "Market Offers",
+      icon: <i class="fa fa-folder" aria-hidden="true"></i>,
+      link:"/business/myoffertable"
+    },
+    {
+      id: 1,
+      label: "Service Settings",
+      icon: <i class="fa fa-cog" aria-hidden="true"></i>,
+      link:"/business/servicesettingsform"
+    },
+  ]
 
-    <div className='side-nav-content'>
+  return(<>
+   <div className='side-nav-content'>
       <div className='side-nav-title ' onClick={toggleSidebar} >
        <p className='side-list-heading ml-4'>MANAGE SERVICES</p> 
        <i class="fa fa-caret-down mr-3 mt-1" aria-hidden="true"></i>
          </div>
          <ul className={`side-nav-links ${sidebarOpen ? 'visible' : 'hidden'}`}>
-      {/* <ul className={'side-nav-links ${toggleSidebarOpen}'}> */}
-      <li className='list-items'>
-      <i class="fa fa-trash mx-4 mt-1" aria-hidden="true"></i>
-      <p className='item-text'>My Offers</p>
-      </li>
-      <li className='list-items'>
+          { ManageServicesItems.map((item)=>(
+          <li 
+          key={item.id}
+          onClick={() => handleNavigation(item.link)}
+          className={location.pathname === item.link ? 'list-item-active' : 'list-items '}>
+            <div className='is-flex py-2 ml-3 side-nav-display-items my-1'>
+            <div className='side-nav-icon px-2 '>{item.icon}</div>
+            <p className='item-text is-size-6'>{item.label}</p>
+            </div>
+        
+          </li>
+     ))}
+  
+      {/* <li className='list-items'>
       <i class="fa fa-pencil-square-o mx-4 mt-1" aria-hidden="true"></i>
       <p className='item-text'>Create Offers</p>
       </li>
@@ -36,11 +75,26 @@ const SideNavigation = () => {
       <li className='list-items'>
       <i class="fa fa-cog mx-4 mt-1" aria-hidden="true"></i>
       <p className='item-text'>Service Settings</p>
-      </li>
+      </li> */}
      
       </ul>
     </div>
-    <div className='side-nav-content'>
+  </>)
+}
+
+const ManageProgram =()=>{
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(sidebarOpen=>!sidebarOpen);
+    console.log('Toggle sidebar')
+  };
+
+  
+
+  return(
+    <>
+      <div className='side-nav-content'>
       <div className='side-nav-title ' onClick={toggleSidebar} >
        <p className='side-list-heading ml-4'>MANAGE PROGRAM</p> 
        <i class="fa fa-caret-down mr-3 mt-1" aria-hidden="true"></i>
@@ -70,7 +124,20 @@ const SideNavigation = () => {
      
       </ul>
     </div>
-    <div className='side-nav-content'>
+    </>
+  )
+}
+
+const MyAccount =() =>{
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(sidebarOpen=>!sidebarOpen);
+    console.log('Toggle sidebar')
+  };
+  return(
+    <>
+     <div className='side-nav-content'>
       <div className='side-nav-title ' onClick={toggleSidebar} >
        <p className='side-list-heading ml-4'>MY ACCOUNT</p> 
        <i class="fa fa-caret-down mr-3 mt-1" aria-hidden="true"></i>
@@ -79,24 +146,34 @@ const SideNavigation = () => {
       {/* <ul className={'side-nav-links ${toggleSidebarOpen}'}> */}
       <li className='list-items'>
       <i class="fa fa-trash mx-4 mt-1" aria-hidden="true"></i>
-      <p className='item-text'>My Offers</p>
+      <p className='item-text'>My CROPs Activity</p>
       </li>
       <li className='list-items'>
       <i class="fa fa-pencil-square-o mx-4 mt-1" aria-hidden="true"></i>
-      <p className='item-text'>Create Offers</p>
+      <p className='item-text'>Statement</p>
       </li>
       <li className='list-items'>
       <i class="fa fa-trash mx-4 mt-1" aria-hidden="true"></i>
-      <p className='item-text'>Market Offers</p>
+      <p className='item-text'>My CROP Card</p>
       </li>
-      <li className='list-items'>
-      <i class="fa fa-cog mx-4 mt-1" aria-hidden="true"></i>
-      <p className='item-text'>Service Settings</p>
-      </li>
+      
      
       </ul>
     </div>
-    <div className='side-nav-content'>
+    </>
+  )
+}
+
+const Settings = ()=>{
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(sidebarOpen=>!sidebarOpen);
+    console.log('Toggle sidebar')
+  };
+  return(
+    <>
+ <div className='side-nav-content'>
       <div className='side-nav-title ' onClick={toggleSidebar} >
        <p className='side-list-heading ml-4'>SETTINGS</p> 
        <i class="fa fa-caret-down mr-3 mt-1" aria-hidden="true"></i>
@@ -105,25 +182,37 @@ const SideNavigation = () => {
       {/* <ul className={'side-nav-links ${toggleSidebarOpen}'}> */}
       <li className='list-items'>
       <i class="fa fa-trash mx-4 mt-1" aria-hidden="true"></i>
-      <p className='item-text'>My Offers</p>
+      <p className='item-text'>Pin Change</p>
       </li>
       <li className='list-items'>
       <i class="fa fa-pencil-square-o mx-4 mt-1" aria-hidden="true"></i>
-      <p className='item-text'>Create Offers</p>
-      </li>
-      <li className='list-items'>
-      <i class="fa fa-trash mx-4 mt-1" aria-hidden="true"></i>
-      <p className='item-text'>Market Offers</p>
-      </li>
-      <li className='list-items'>
-      <i class="fa fa-cog mx-4 mt-1" aria-hidden="true"></i>
-      <p className='item-text'>Service Settings</p>
+      <p className='item-text'>Communication Preference</p>
       </li>
      
       </ul>
     </div>
 
+    </>
+  )
+}
 
+const SideNavigation = () => {
+
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+      setSidebarOpen(sidebarOpen=>!sidebarOpen);
+      console.log('Toggle sidebar')
+    };
+
+    
+  return (
+   <>
+   <div className='main-side-nave-items'>
+      <ManageServices />
+      <ManageProgram />
+      <MyAccount />
+      <Settings />
    </div>
    </>
   )
