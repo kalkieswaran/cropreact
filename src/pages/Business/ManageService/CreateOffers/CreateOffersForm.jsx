2a1970d82081c5f60df80b7297a907c26d82b53e
kalkieswaran <155619180+kalkieswaran@users.noreply.github.com>
2024-02-06 16:14:13 +0530

    ok

--- src/pages/Business/ManageService/CreateOffers/CreateOffersForm.jsx
+++ src/pages/Business/ManageService/CreateOffers/CreateOffersForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+<<<<<<< Updated upstream
 const CreateOffersForm = () => {
   return (
     <div>CreateOffersForm</div>
@@ -7,3 +8,48 @@ const CreateOffersForm = () => {
 }
 
 export default CreateOffersForm
+=======
+
+const CreateOffersForm = () => {
+
+    const formElements = [
+        {
+            type: "TEXT_BOX",
+            width: "3",
+            name: "input_one",
+            element: {
+                label: "Enter Name",
+                validations: numericValidations
+            }
+        },
+        {
+            type: "TEXT_BOX",
+            width: "3",
+            name: "input_two",
+            element: {
+                label: "Enter ID",
+                validations: numericValidations
+            }
+        },
+        {
+            type: "SELECT_BOX",
+            width: "3",
+            name: "select_one",
+            element: {
+                label: "Select Role",
+                options: options
+            }
+        }
+    ]
+
+    return (
+        <div className='container'>
+            <SmartSoftForm
+                elements={formElements}
+            />
+        </div>
+    )
+}
+
+export default CreateOffersForm
+>>>>>>> Stashed changes
