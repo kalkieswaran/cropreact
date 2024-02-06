0b6c62e85ac59382a0d4529f8a00fcd7177f6d75
kalkieswaran <155619180+kalkieswaran@users.noreply.github.com>
2024-02-06 15:35:44 +0530

    kalki

--- src/pages/Business/ManageService/CreateOffers/CreateOffersForm.jsx
+++ src/pages/Business/ManageService/CreateOffers/CreateOffersForm.jsx
@@ -1,25 +1,162 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { SmartSoftButton, SmartSoftForm } from 'soft_digi';
+import SmartForm from 'soft_digi/dist/forms/SmartForm';
 
 
 const CreateOffersForm = () => {
-<<<<<<< Updated upstream
+  const [formData, setFormData] = useState({});
+  const [formSubmit, setFormSubmit] = useState(false);
+  const [formErrors, setFormErrors] = useState(false);
+  const [type, setType] = useState("password");
+
+  const handleInputChange = (name, value) => {
+    //console.log(" name " ,name , "   value " , value)
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleErrorChange = (name, value) => {
+    setFormErrors((prev) => {
+      // Create a copy of the previous state
+      const updatedFormData = { ...prev };
+      // Check if the value is null or empty
+      if (value === null || value === "") {
+        // If true, remove the property from the updated state
+        delete updatedFormData[name];
+      } else {
+        // Otherwise, update or add the property in the updated state
+        updatedFormData[name] = value;
+      }
+
+      return updatedFormData;
+    });
+  };
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
+  const handleFormSubmit = () => {
+    console.log("formdata", formData);
+    setFormSubmit(true);
+    console.log("form errors ", formErrors);
+  };
+
+  const numericValidations = [
+    {
+      type: "required",
+      msg: "Numeric Value is Required",
+    },
+    {
+      type: "pattern",
+      msg: "Please Enter Correct Numeric Value",
+      pattern: "[0-9]+",
+    },
+  ];
+
+  const formElements = [
+    {
+      type: "TEXT_BOX",
+      width: "4",
+      name: "input_one",
+      element: { placeHolder: 'Title', validations: numericValidations }
+    },
+    {
+      type: "TEXT_BOX",
+      width: "4",
+      name: "input_two",
+      element: { placeHolder: 'Brand', validations: numericValidations }
+    },
+    {
+      type: "SELECT_BOX",
+      width: "4",
+      name: "select_three",
+      element: { placeHolder: 'Sector ', options: options }
+    },
+    {
+      type: "SELECT_BOX",
+      width: "4",
+      name: "select_four",
+      element: { placeHolder: 'Apply', options: options }
+    },
+    {
+      type: "TEXT_BOX",
+      width: "4",
+      name: "input_five",
+      element: { placeHolder: 'Original Price', validations: numericValidations }
+    },
+    {
+      type: "TEXT_BOX",
+      width: "4",
+      name: "input_six",
+      element: { placeHolder: 'Discount Percentage', validations: numericValidations }
+    },
+    {
+      type: "TEXT_BOX",
+      width: "4",
+      name: "input_seven",
+      element: { placeHolder: 'Quentity', validations: numericValidations }
+    },
+    {
+      type: "TEXT_BOX",
+      width: "4",
+      name: "input_eight",
+      element: { placeHolder: 'Product Available From Date', validations: numericValidations }
+    },
+    {
+      type: "TEXT_BOX",
+      width: "4",
+      name: "input_nine",
+      element: { placeHolder: 'Detailed Description', validations: numericValidations }
+    },
+    {
+      type: "TEXT_BOX",
+      width: "4",
+      name: "input_ten",
+      element: { placeHolder: 'Brief Description', validations: numericValidations }
+    },
+    {
+      type: "TEXT_BOX",
+      width: "4",
+      name: "input_eleven",
+      element: { placeHolder: 'Brief Description', validations: numericValidations }
+    },
+
+  ]
+
   return (
     <div className='CreateOffersForm'>
-      
+      <div className='header is-size-5 has-text-link'>
+        <i class="fa fa-caret-square-o-left ml-3 mr-5 " aria-hidden="true"></i>
+
+        Amend Products
+
+      </div>
+      <hr />
+      <SmartSoftForm formData={formData}
+        setFormData={handleInputChange}
+        elements={formElements}
+        formSubmit={formSubmit}
+        handleErrorChange={handleErrorChange} />
+
+
+      <div className='is-justify-content-space-between is-flex'>
+        <span> <SmartSoftButton
+          label="submit"
+          classList={["is-primary", "is-small", "is-rounded"]}
+        />
+        </span>
+        <span>
+          <SmartSoftButton
+            label="View Offers"
+            classList={["is-primary", "is-small", "is-rounded"]}
+          />
+          <SmartSoftButton
+            label="Moeket Offer"
+            classList={["is-primary", "is-small", "is-rounded"]}
+          />
+        </span>
+      </div>
     </div>
   )
-}
-=======
->>>>>>> Stashed changes
-
-
-  return (
-
-    <div className='CreateOffersForm '>
-      mmnknlmok
-    </div>
-
-  );
 };
 
 export default CreateOffersForm;
--- src/pages/Business/PromosButton/CreatePromosOfferForm.jsx
+++ src/pages/Business/PromosButton/CreatePromosOfferForm.jsx
@@ -1,10 +1,4 @@
 import React, { useState } from 'react'
-<<<<<<< Updated upstream
-import { SmartSoftButton, SmartSoftForm } from 'soft_digi';
-
-const CreatePromosOfferForm = () => {
-
-=======
 import { SmartSoftButton, SmartSoftDate, SmartSoftForm } from "soft_digi"
 
 
@@ -12,7 +6,6 @@ import { SmartSoftButton, SmartSoftDate, SmartSoftForm } from "soft_digi"
 const CreatePromosOfferForm = () => {
 
 
->>>>>>> Stashed changes
   const [formData, setFormData] = useState({});
   const [formSubmit, setFormSubmit] = useState(false);
   const [formErrors, setFormErrors] = useState(false);
@@ -38,8 +31,6 @@ const CreatePromosOfferForm = () => {
       return updatedFormData;
     });
   };
-<<<<<<< Updated upstream
-=======
   const options = [
     { value: "chocolate", label: "Chocolate" },
     { value: "strawberry", label: "Strawberry" },
@@ -50,7 +41,6 @@ const CreatePromosOfferForm = () => {
     setFormSubmit(true);
     console.log("form errors ", formErrors);
   };
->>>>>>> Stashed changes
 
   const numericValidations = [
     {
@@ -64,187 +54,76 @@ const CreatePromosOfferForm = () => {
     },
   ];
 
-<<<<<<< Updated upstream
-  const handleFormSubmit = () => {
-    console.log("formdata", formData);
-    setFormSubmit(true);
-    console.log("form errors ", formErrors);
-  };
-
-  const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
-  ];
-
-
-=======
->>>>>>> Stashed changes
   const formElements = [
     {
       type: "TEXT_BOX",
       width: "4",
       name: "input_one",
-<<<<<<< Updated upstream
-      element: {
-        placeHolder: 'Uplosd Product Images',
-        validations: numericValidations
-      }
-=======
       element: { placeHolder: 'Upload Product Image', validations: numericValidations }
->>>>>>> Stashed changes
     },
     {
       type: "TEXT_BOX",
       width: "4",
       name: "input_two",
-<<<<<<< Updated upstream
-      element: {
-        placeHolder: 'Base Price',
-        validations: numericValidations
-      }
-    },
-    {
-      type: "TEXT_BOX",
-      width: "4",
-      name: "input_three",
-      element: {
-        placeHolder: 'Product Available From Date',
-        validations: numericValidations
-      }
-=======
       element: { placeHolder: 'Base Price(AUD)', validations: numericValidations }
     },
     {
       type: "TEXT_BOX",
       width: "4", name:
-        "input_three", 
-        element:{ placeHolder: 'Product Availlable from', validations: numericValidations}
->>>>>>> Stashed changes
+        "input_three",
+      element: { placeHolder: 'Product Availlable from', validations: numericValidations }
     },
     {
       type: "TEXT_BOX",
       width: "4",
       name: "input_four",
-<<<<<<< Updated upstream
-      element: {
-        placeHolder: 'Title',
-        validations: numericValidations
-      }
-=======
       element: { placeHolder: 'Title', validations: numericValidations }
->>>>>>> Stashed changes
     },
     {
       type: "TEXT_BOX",
       width: "4",
       name: "input_five",
-<<<<<<< Updated upstream
-      element: {
-        placeHolder: 'Briel Description Required',
-        validations: numericValidations
-      }
-=======
       element: { placeHolder: 'Brisf Description Required', validations: numericValidations }
->>>>>>> Stashed changes
     },
     {
       type: "TEXT_BOX",
       width: "4",
       name: "input_six",
-<<<<<<< Updated upstream
-      element: {
-        placeHolder: 'Slot',
-        validations: numericValidations
-      }
-=======
       element: { placeHolder: 'Slot', validations: numericValidations }
->>>>>>> Stashed changes
     },
     {
       type: "TEXT_BOX",
       width: "4",
       name: "input_seven",
-<<<<<<< Updated upstream
-      element: {
-        placeHolder: 'Quantity',
-        validations: numericValidations
-      }
-=======
       element: { placeHolder: 'Quantity', validations: numericValidations }
->>>>>>> Stashed changes
     },
     {
       type: "TEXT_BOX",
       width: "4",
       name: "input_eight",
-<<<<<<< Updated upstream
-      element: {
-        placeHolder: 'Customize Message',
-        validations: numericValidations
-      }
-=======
       element: { placeHolder: 'Customize Message', validations: numericValidations }
->>>>>>> Stashed changes
     },
     {
       type: "TEXT_BOX",
       width: "4",
       name: "input_nine",
-<<<<<<< Updated upstream
-      element: {
-        placeHolder: 'Publish End Data',
-        validations: numericValidations
-      }
-=======
       element: { placeHolder: 'Publish End Date', validations: numericValidations }
->>>>>>> Stashed changes
     },
     {
       type: "TEXT_BOX",
       width: "4",
       name: "input_ten",
-<<<<<<< Updated upstream
-      element: {
-        placeHolder: 'Product Available To Date',
-        validations: numericValidations
-      }
-=======
       element: { placeHolder: 'prodct Available To Date', validations: numericValidations }
->>>>>>> Stashed changes
     },
     {
       type: "SELECT_BOX",
       width: "4",
       name: "select_eleven",
-<<<<<<< Updated upstream
-      element: {
-        placeHolder: 'Sector',
-        options: options
-      }
-=======
       element: { placeHolder: 'Sector', options: options }
->>>>>>> Stashed changes
     },
     {
       type: "TEXT_BOX",
       width: "4",
-<<<<<<< Updated upstream
-      name: "input_towl",
-      element: {
-        placeHolder: 'Bid Price(AUD)',
-        validations: numericValidations
-      }
-    },
-    {
-      type: "SELECT_BOX",
-      width: "4",
-      name: "select_tharteen",
-      element: {
-        placeHolder: 'Publish Available To Date',
-        options: options
-      }
-=======
       name: "input_twelve",
       element: {
         placeHolder: 'Bid Price (AUO)',
@@ -257,50 +136,28 @@ const CreatePromosOfferForm = () => {
       width: "4",
       name: "select_thirteen",
       element: { placeHolder: 'Publish Start Date', options: options }
->>>>>>> Stashed changes
     },
     {
       type: "SELECT_BOX",
       width: "4",
       name: "select_fourteen",
-<<<<<<< Updated upstream
-      element: {
-        placeHolder: 'Apply',
-        options: options
-      }
-=======
       element: { placeHolder: 'Apply ', options: options }
->>>>>>> Stashed changes
     },
     {
       type: "TEXT_BOX",
       width: "4",
-<<<<<<< Updated upstream
-      name: "input_fourteen",
-      element: {
-        placeHolder: 'Detailed description',
-        validations: numericValidations
-      }
-=======
       name: "input_fifteen",
       element: { placeHolder: 'Detailed ', validations: numericValidations }
->>>>>>> Stashed changes
     },
   ]
 
   return (
     <div className='CreatePromosOfferForm'>
-<<<<<<< Updated upstream
-      <div className='header has-text-link is-size-5'>
-        <i class="fa fa-caret-square-o-left ml-3 mr-5" aria-hidden="true"></i>
-        Create Promo Offer
-=======
       <div className='header is-size-5 has-text-link'>
         <i class="fa fa-caret-square-o-left ml-3 mr-5 " aria-hidden="true"></i>
 
         Create Promo Offer
 
->>>>>>> Stashed changes
       </div>
       <hr />
       <SmartSoftForm formData={formData}
@@ -309,28 +166,13 @@ const CreatePromosOfferForm = () => {
         formSubmit={formSubmit}
         handleErrorChange={handleErrorChange} />
 
-<<<<<<< Updated upstream
-      <div className='is-flex is-justify-content-center'>
-        <SmartSoftButton
-          label="submit"
-          classList={["is-primary", "is-small", "is-rounded",]}
-          onClick={() => handleFormSubmit()}
-=======
 
-      <SmartSoftDate key="text-4"
-        label="Enter Employee Name"
-        value={formData?.input_four || ""}
-        onChange={(value) => handleInputChange("input_four", value)}
-        inputType="BORDER_LABEL_FOCUS"
-        leftIcon="fa-user"
-        inputProps={{ isFocussed: true }}
-      />
+
 
       <div className='is-justify-content-center is-flex'>
         <SmartSoftButton
           label="submit"
           classList={["is-primary", "is-small", "is-rounded"]}
->>>>>>> Stashed changes
         />
       </div>
     </div>
